850fa548c5c9764cdd42eedcc753c4ed568560a1
kyle edwards <126681707+bobbobson21@users.noreply.github.com>
2024-02-29 16:09:44 +0000

    drhrfthn

--- EngineContent/EngineSettings/EngineSettings.h
+++ EngineContent/EngineSettings/EngineSettings.h
@@ -24,11 +24,7 @@ namespace EngineSettings
 		static BaseEntity* CommandConsoleCMD = new ConsoleReciver(); //the console //PS this is an entity because they can store stuff othere ents and components may want to access in KV formating but this ent is also never validated meaning it has to be destroy manually
 		//technically CommandConsoleCMD is not const but technically it cant change during run time
 
-#if DEBUG
 		static bool ConsoleAllowed = true;
-#else
-		static bool ConsoleAllowed = false;//just encase you wanted the console to be off by default in game
-#endif
 	}
 
 	bool ChangeInRunTime(string Var, int Value); //do note not all values should be updated dureing runtime so not all values are
@@ -37,6 +33,6 @@ namespace EngineSettings
 	int GetUpToDateValue(string Var, int TYPEID);
 	bool GetUpToDateValue(string Var, bool TYPEID);
 
-	int GetConstValue(string Var, int TYPEID);
+	int GetConstValue(string Var, int TYPEID); //these vauses can not be changed in run time 
 	BaseEntity *GetConstValue(string Var, BaseEntity TYPEID);
 }
